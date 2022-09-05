3e8f33deb5f7d8a789a5a9529b95421c74536e7b
ShettyGautami <112805468+ShettyGautami@users.noreply.github.com>
2022-09-05 11:19:21 +0530

    Add files via upload

---
+++ Basic_Programs/05_A_Program_for_swapping_two_numbers_using_temp_.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int ino1 = 0, ino2 = 0, temp = 0;
+
+    printf("\nBefore swapping =>");
+    printf("\nEnter first number : ");
+    scanf("%d",&ino1);
+    printf("\nEnter second number : ");
+    scanf("%d",&ino2);
+
+    temp = ino1;
+    ino1 = ino2;
+    ino2 = temp;
+    
+    printf("\nAfter swapping =>");
+    printf("\nEnter first number : %d",ino1);
+    printf("\nEnter second number : %d",ino2);
+
+    printf("\nThanks for visiting");
+    getch();
+    return 0;
+}
