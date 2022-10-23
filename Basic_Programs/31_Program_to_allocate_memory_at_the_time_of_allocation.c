1e5d8bc3435f46ad78c4b293220222dec71eeb6a
ShettyGautami <112805468+ShettyGautami@users.noreply.github.com>
2022-10-23 23:37:18 +0530

    Add files via upload

---
+++ Basic_Programs/31_Program_to_allocate_memory_at_the_time_of_allocation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n = 0, avg = 0, i = 0, *p = 0, sum = 0;
+
+    printf("\nEnter the number of students : ");
+    scanf("%d");
+    getch();
+    return 0;
+}
